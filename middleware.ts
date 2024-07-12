dc23e1c8b134fec774e72f828e54be32ec1fe126
RidloGhifary <ridloghfry@gmail.com>
2024-07-12 18:58:25 +0700

    feat: protect some page using middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/api/webhook/clerk"]);
-const isIgnoredRoute = createRouteMatcher(["/api/webhook/clerk"]);
+const isProtectedRoute = createRouteMatcher(["/"]);
 
-export default clerkMiddleware();
+export default clerkMiddleware((auth, req: NextRequest) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    // Add custom logic to run before redirecting
+
+    return auth().redirectToSignIn();
+  }
+
+  if (auth().userId && !isProtectedRoute(req)) {
+    if (req.nextUrl.pathname === "/sign-in" && auth().userId) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
